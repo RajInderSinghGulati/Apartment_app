51eab77b26af3355f3f731c8c7e1228d2236616c
Raj Inder Singh Gulati <rajisflash@Rajs-MacBook-Air-2.local>
2025-06-14 00:43:38 +0530

    Updated apis

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -65,10 +65,10 @@ exports.deleteAdmin = async (req, res) => {
 exports.loginAdmin = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const admin = await Admin.findOne({ email });
-    if (!admin) return res.status(401).json({ error: "Invalid credentials" });
+    const admin = await Admin.findOne({ email : email });
+    if (!admin) return res.status(401).json({ error: "Invalid credentials (email)" });
     const isMatch = await bcrypt.compare(password, admin.password);
-    if (!isMatch) return res.status(401).json({ error: "Invalid credentials" });
+    if (!isMatch) return res.status(401).json({ error: "Invalid credentials(password)" });
     const token = jwt.sign({ adminId: admin._id, email: admin.email, role: "admin" }, process.env.JWT_SECRET, { expiresIn: '1h' });
     res.status(200).json({ token });
   } catch (err) {
